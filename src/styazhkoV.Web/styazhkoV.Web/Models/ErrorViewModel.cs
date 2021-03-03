6edb499b2d168bdf1776fa73296ed59557c94069
Alex Snow <32740603+styazhkoV@users.noreply.github.com>
2021-03-03 10:37:26 +0600

    https://www.youtube.com/watch?v=jwzgp6lyvYQ&list=PLIB8be7sunXPK-sm4dTXaTsL5KICB3JnS&index=2&ab_channel=SergeiCalabonga

---
+++ src/styazhkoV.Web/styazhkoV.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace styazhkoV.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
